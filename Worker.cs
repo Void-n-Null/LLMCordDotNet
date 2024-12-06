dabf40ccd750cc4a975ecdab139167a7484b518e
BoneGearsSoftware <70048414+Void-n-Null@users.noreply.github.com>
2024-12-05 19:16:36 -0600

    Initial Commit
    Add project structure for LLMCordDotNet including Docker configuration, application settings, and core files. Introduced .dockerignore and .gitignore files, along with essential project files such as Dockerfile, appsettings, and main application code.

---
+++ Worker.cs
@@ -0,0 +1,24 @@
+namespace LLMCordDotNet;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Void Null Was Here 😎");
+            }
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
